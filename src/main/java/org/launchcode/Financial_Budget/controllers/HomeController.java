e62fde6b746b947322ea432724c6e2cdf5dd2bed
DeepthiPallithazhath <deepthips87@gmail.com>
2021-04-25 08:03:24 -0500

    first commit

--- src/main/java/org/launchcode/Financial_Budget/controllers/HomeController.java
+++ src/main/java/org/launchcode/Financial_Budget/controllers/HomeController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     public String helloWithQueryParam(@RequestParam String name, Model model) {
         String greeting = "Hello, " + name + "!";
         model.addAttribute("greeting", greeting);
         return "index";
     }
+
 }
