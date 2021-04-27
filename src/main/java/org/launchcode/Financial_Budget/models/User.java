9132e88a03812b9b84f949b0589d69ed5d877f07
Josh <blkjosh923@gmail.com>
2021-04-26 22:55:30 -0500

    "Authentication halfway finished"

---
+++ src/main/java/org/launchcode/Financial_Budget/models/User.java
@@ -0,0 +1,34 @@
+package org.launchcode.Financial_Budget.models;
+
+import com.sun.istack.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class User extends AbstractEntity {
+
+
+    private String username;
+
+
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public User() {}
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+}
---
+++ src/main/java/org/launchcode/Financial_Budget/models/data/dto/RegisterFormDTO.java
@@ -0,0 +1,15 @@
+package org.launchcode.Financial_Budget.models.data.dto;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
