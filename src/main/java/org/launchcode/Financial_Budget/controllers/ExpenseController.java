5b525f2e9b83e35766e6d42ffa8a970f16c12cc8
DeepthiPallithazhath <deepthips87@gmail.com>
2021-05-12 12:56:35 -0500

    t sts

--- src/main/java/org/launchcode/Financial_Budget/controllers/ExpenseController.java
+++ src/main/java/org/launchcode/Financial_Budget/controllers/ExpenseController.java
@@ -1,10 +1,13 @@
 package org.launchcode.Financial_Budget.controllers;
 
+import org.launchcode.Financial_Budget.models.Category;
+import org.launchcode.Financial_Budget.models.Expense;
 import org.launchcode.Financial_Budget.models.User;
 import org.launchcode.Financial_Budget.models.data.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +19,8 @@ public class ExpenseController {
     @Autowired
     private CategoryRepository categoryRepository;
     @Autowired
+    private CategoryRepository expenseRepository;
+    @Autowired
     AunthenticationController authenticationController;
 
     @RequestMapping("/expense-view")
@@ -25,4 +30,11 @@ public class ExpenseController {
         //model.addAttribute("categoryList", categoryRepository.findAllByUsers_Id(user.getId()));
         return "viewExpense";
     }
+    @GetMapping("/addExpense")
+    public String addExpense(Model model) {
+        Expense expense=new Expense();
+        model.addAttribute("expense", expense);
+        return "addExpense";
+    }
+
 }
---
+++ src/main/java/org/launchcode/Financial_Budget/models/data/ExpenseRepository.java
@@ -0,0 +1,15 @@
+package org.launchcode.Financial_Budget.models.data;
+
+import org.launchcode.Financial_Budget.models.Category;
+import org.launchcode.Financial_Budget.models.Expense;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
+
+
+
+}
