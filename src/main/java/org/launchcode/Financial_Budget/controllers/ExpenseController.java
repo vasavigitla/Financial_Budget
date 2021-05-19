75a548cb73dbab816d15f52ffb1d7977d98b2bdf
DeepthiPallithazhath <deepthips87@gmail.com>
2021-05-19 12:28:05 -0500

    t sts

--- src/main/java/org/launchcode/Financial_Budget/controllers/ExpenseController.java
+++ src/main/java/org/launchcode/Financial_Budget/controllers/ExpenseController.java
@@ -50,31 +50,28 @@ public class ExpenseController {
     public String showFormForExpenseUpdate(@PathVariable (value="id") int id, Model model) {
         Expense expense=getExpenseById(id);
         model.addAttribute("expense", expense);
-        return "addExpense";
+        return "updateExpense";
+    }
+
+    @GetMapping("/deleteExpense/{id}")
+    public String deleteExpense(@PathVariable (value="id") int id, Model model,HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        Expense expense=getExpenseById(id);
+        expenseRepository.delete(expense);
+        model.addAttribute("expenseList", expenseRepository.findAllByUsers_Id(user.getId()));
+        return "viewExpense";
     }
 
     @PostMapping("/saveExpense")
     public String saveCategory(@ModelAttribute @Valid Expense expense,
                                Errors errors, Model model, @RequestParam int categoryId,HttpServletRequest request) {
-        if (errors.hasErrors()) {
+            if (errors.hasErrors()) {
             model.addAttribute("title", "Add Expense");
             return "addExpense";
-        }
-
-      /*  if(expense.getId()!=0) {
+            }
             HttpSession session = request.getSession();
             User user = authenticationController.getUserFromSession(session);
-            //expense.setUsers(user);
-            Expense updateExpense = new Expense();
-            updateCategory.setId(category.getId());
-            updateCategory.setCategoryName(category.getCategoryName());
-            updateCategory.setCategoryDescription(category.getCategoryDescription());
-            updateCategory.setUsers(category.getUsers());
-            model.addAttribute("categoryList", this.categoryRepository.save(updateCategory));
-            model.addAttribute("categoryList", categoryRepository.findAllByUsers_Id(user.getId()));
-        }*/
-             HttpSession session = request.getSession();
-             User user = authenticationController.getUserFromSession(session);
             Optional optCategory = categoryRepository.findById(categoryId);
             if (optCategory.isPresent()) {
             Category category = (Category) optCategory.get();
@@ -90,6 +87,26 @@ public class ExpenseController {
          }
 
       }
+
+    @PostMapping("/updateExpense")
+    public String updateExpense(@ModelAttribute @Valid Expense expense,
+                               Errors errors, Model model, HttpServletRequest request) {
+
+           if(expense.getId()!=0) {
+            HttpSession session = request.getSession();
+            User user = authenticationController.getUserFromSession(session);
+            expense.setUsers(user);
+            Optional<Expense> updateExpenseOptional = this.expenseRepository.findById(expense.getId());
+               if(updateExpenseOptional.isPresent()) {
+                   Expense updateExpense = updateExpenseOptional.get();
+                   updateExpense.setExpense_amount(expense.getExpense_amount());
+                   model.addAttribute("expenseList", this.expenseRepository.save(updateExpense));
+               }
+            model.addAttribute("expenseList", this.expenseRepository.findAllByUsers_Id(user.getId()));
+        }
+
+    return "viewExpense";
+    }
 
     public Expense getExpenseById(int id)
     {
